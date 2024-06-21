74261b484390910ed31bb56a973727fd8294a77d
Carlos Mestre <carmesce@gmail.com>
2024-06-21 11:15:31 +0200

    Potencia y logaritmo

--- index.js
+++ index.js
@@ -21,8 +21,35 @@ const resta = (req, res) => {
     res.send(`${Number(req.query.parametro1) - Number(req.query.parametro2)}`)
 }
 
+const multiplica = (req, res) => {
+    res.send(`${Number(req.query.parametro1) * Number(req.query.parametro2)}`)
+}
+
+const divide = (req, res) => {
+    res.send(`${Number(req.query.parametro1) / Number(req.query.parametro2)}`)
+}
+
+const potencia = (req, res) => {
+    res.send(`${Number(req.query.parametro1) ** Number(req.query.parametro2)}`)
+    // res.send(`${Math.pow(Number(req.query.parametro1), Number(req.query.parametro2))}`)
+}
+
+const log = (req, res) => {
+    res.send(`${Math.log(Number(req.query.parametro1), Number(req.query.parametro2))}`)
+
+}
+
+const ecuacion = (req, res) => {
+    res.send(400)
+}
+
 calculadoraBackend.get("/suma", suma)
 calculadoraBackend.get("/resta", resta)
+calculadoraBackend.get("/multiplica", multiplica)
+calculadoraBackend.get("/divide", divide)
+calculadoraBackend.get("/potencia", potencia)
+calculadoraBackend.get("/log", log)
+calculadoraBackend.get("/ecuacion", ecuacion)
 
 calculadoraBackend.listen(port, () => {
     console.log(`Calculadora escuchando en http://localhost:${port}`)
