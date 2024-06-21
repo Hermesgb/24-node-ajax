7e6dfb5b6dda31e5e5ec7589fd9330975f0337f6
Carlos Mestre <carmesce@gmail.com>
2024-06-21 11:22:06 +0200

    Code style

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ const express = require("express")
 const calculadoraBackend = express()
 const port = 3000
 
+// registramos el contenido estatico: asi se sirven los ficheros al navegador
 calculadoraBackend.use(express.static('static/html'))
 calculadoraBackend.use(express.static('static/js'))
 
-calculadoraBackend.get("/hola", (req, res) =>{
-    res.send("Hola Calculadora")
-})
-
+// Accion y enrutado separado para que se vea como se conecta el endpoint suma con su accion
 const suma = (req, res) => {
     var sumando1 = Number(req.query.parametro1)
     var sumando2 = Number(req.query.parametro2)
@@ -17,40 +15,34 @@ const suma = (req, res) => {
     res.send(`${resultado}`)
 }
 
-const resta = (req, res) => {
-    res.send(`${Number(req.query.parametro1) - Number(req.query.parametro2)}`)
-}
-
-const multiplica = (req, res) => {
-    res.send(`${Number(req.query.parametro1) * Number(req.query.parametro2)}`)
-}
-
-const divide = (req, res) => {
-    res.send(`${Number(req.query.parametro1) / Number(req.query.parametro2)}`)
-}
-
-const potencia = (req, res) => {
-    res.send(`${Number(req.query.parametro1) ** Number(req.query.parametro2)}`)
-    // res.send(`${Math.pow(Number(req.query.parametro1), Number(req.query.parametro2))}`)
-}
-
-const log = (req, res) => {
-    res.send(`${Math.log(Number(req.query.parametro1), Number(req.query.parametro2))}`)
-
-}
-
-const ecuacion = (req, res) => {
-    res.send(400)
-}
-
 calculadoraBackend.get("/suma", suma)
-calculadoraBackend.get("/resta", resta)
-calculadoraBackend.get("/multiplica", multiplica)
-calculadoraBackend.get("/divide", divide)
-calculadoraBackend.get("/potencia", potencia)
-calculadoraBackend.get("/log", log)
-calculadoraBackend.get("/ecuacion", ecuacion)
 
+// Resto de acciones más compactadas:
+calculadoraBackend.get("/resta", (req, res) => {
+    res.send(`${Number(req.query.parametro1) - Number(req.query.parametro2)}`)
+})
+
+calculadoraBackend.get("/multiplica", (req, res) => {
+    res.send(`${Number(req.query.parametro1) * Number(req.query.parametro2)}`)
+})
+
+calculadoraBackend.get("/divide", (req, res) => {
+    res.send(`${Number(req.query.parametro1) / Number(req.query.parametro2)}`)
+})
+
+calculadoraBackend.get("/potencia", (req, res) => {
+    res.send(`${Number(req.query.parametro1) ** Number(req.query.parametro2)}`)
+})
+
+calculadoraBackend.get("/log", log = (req, res) => {
+    res.send(`${Math.log(Number(req.query.parametro1), Number(req.query.parametro2))}`)
+})
+
+calculadoraBackend.get("/ecuacion", ecuacion = (req, res) => {
+    res.send(400)
+})
+
+// Escuchamos, si no escuchamos el servidor no responde
 calculadoraBackend.listen(port, () => {
     console.log(`Calculadora escuchando en http://localhost:${port}`)
 })
